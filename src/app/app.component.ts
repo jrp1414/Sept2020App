8e25a67979f74c465f19c10586f247aaad67592e
Jyotiranjan Panda <jyotiranjan.p14@gmail.com>
2020-09-15 09:21:09 +0530

    Initial Setup of application 15 Sept 2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
+import { TemplateParseResult } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // selector: 'app-main',
+  // selector: '[app-main]',
+  selector: '.app-main',
+  // template:"<h1>Inline Template Example</h1>"+
+  // "<h2>Inline Template Example 2</h2>"+
+  // "<h3>Header 3</h3>"  
+  // template:`
+  // <h1>Multiple Line Temple Example</h1>
+  // <h2>Multiple Line Temple Example</h2>
+  // <h3>Multiple Line Temple Example</h3>
+  // `
+  templateUrl:"./app.component.html",
+  // styles:[
+  //   `h1{background-color:yellow}`,
+  //   `h2{background-color:blue}`
+  // ]
+  styleUrls:[
+    './app.component.css'
+  ]
 })
 export class AppComponent {
   
