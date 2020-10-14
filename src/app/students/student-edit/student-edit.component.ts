fa26c03cca00c6e1b2c488800e7242d6d826df06
Jyotiranjan Panda <jyotiranjan.p14@gmail.com>
2020-10-14 10:01:05 +0530

    Reactive Form example 4

--- src/app/students/student-edit/student-edit.component.ts
+++ src/app/students/student-edit/student-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-student-edit',
@@ -9,39 +9,26 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
 export class StudentEditComponent implements OnInit {
 
   studentEditForm : FormGroup;
-  firstName:FormControl= new FormControl();
-  lastName:FormControl= new FormControl();
-  emailId:FormControl= new FormControl();
-  mobileNo:FormControl= new FormControl();
-  addLine1:FormControl= new FormControl();
-  addLine2:FormControl= new FormControl();
-  addLine3:FormControl= new FormControl();
-  city:FormControl= new FormControl();
-  state:FormControl= new FormControl();
-  address: FormGroup;
-  hobbies:FormArray = new FormArray([
-    new FormControl()
-  ]);
   
-
-
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
-    this.studentEditForm = new FormGroup({
-      firstName: this.firstName,
-      lastName:this.lastName,
-      emailId:this.emailId,
-      mobileNo:this.mobileNo,
-      address:new FormGroup({
-        addLine1: this.addLine1,
-        addLine2: this.addLine2,
-        addLine3: this.addLine3,
-        city:this.city,
-        state:this.state
+    this.studentEditForm = this.fb.group({
+      firstName:this.fb.control(""),
+      lastName:this.fb.control(""),
+      emailId:this.fb.control(""),
+      mobileNo:this.fb.control(""),
+      address:this.fb.group({
+        addLine1:this.fb.control(""),
+        addLine2:this.fb.control(""),
+        addLine3:this.fb.control(""),
+        city:this.fb.control(""),
+        state:this.fb.control("")
       }),
-      hobbies: this.hobbies 
-    });
+      hobbies:this.fb.array([
+        this.fb.control("")
+      ])
+    }) 
   }
 
   OnSubmit(){
@@ -50,7 +37,7 @@ export class StudentEditComponent implements OnInit {
   }
 
   AddHobby(){
-    this.hobbies.push(new FormControl());
+    (<FormArray>this.studentEditForm.get("hobbies")).push(this.fb.control(""));
   }
 
 }
