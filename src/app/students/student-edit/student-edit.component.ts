855375adc9ad7221cc1c07e7650e3c1ce4b21428
Jyotiranjan Panda <jyotiranjan.p14@gmail.com>
2020-10-15 09:13:02 +0530

    Reactive Forms Example 4

--- src/app/students/student-edit/student-edit.component.ts
+++ src/app/students/student-edit/student-edit.component.ts
@@ -1,33 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-student-edit',
   templateUrl: './student-edit.component.html',
-  styles: []
+  styles: [
+    // `
+    // input.ng-invalid.ng-touched {
+    //   border-color:darkred;
+    //   border-width: 5px;
+    // }
+    // `
+  ]
 })
 export class StudentEditComponent implements OnInit {
 
   studentEditForm : FormGroup;
-  
+  hobbies:FormArray;
+  addresses:FormArray;
   constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
-    this.studentEditForm = this.fb.group({
-      firstName:this.fb.control(""),
-      lastName:this.fb.control(""),
-      emailId:this.fb.control(""),
-      mobileNo:this.fb.control(""),
-      address:this.fb.group({
+    this.hobbies = this.fb.array([
+      this.fb.control("")
+    ]);
+    this.addresses = this.fb.array([
+      this.fb.group({
         addLine1:this.fb.control(""),
         addLine2:this.fb.control(""),
-        addLine3:this.fb.control(""),
+        addLine3:this.fb.control(null),
         city:this.fb.control(""),
         state:this.fb.control("")
-      }),
-      hobbies:this.fb.array([
-        this.fb.control("")
-      ])
+      })
+    ])
+    this.studentEditForm = this.fb.group({
+      firstName:this.fb.control("",[Validators.required,Validators.minLength(3),Validators.maxLength(12)]),
+      lastName:this.fb.control("",Validators.required),
+      emailId:this.fb.control("",[Validators.required,Validators.email]),
+      mobileNo:this.fb.control(""),
+      notificationType:this.fb.control("email"),
+      termsAndConditions:this.fb.control(""),
+      addresses:this.addresses,
+      hobbies:this.hobbies
     }) 
   }
 
@@ -38,6 +52,25 @@ export class StudentEditComponent implements OnInit {
 
   AddHobby(){
     (<FormArray>this.studentEditForm.get("hobbies")).push(this.fb.control(""));
+  }
+  AddAddress(){
+    (<FormArray>this.studentEditForm.get("addresses")).push(this.fb.group({
+      addLine1:this.fb.control(""),
+      addLine2:this.fb.control(""),
+      addLine3:this.fb.control(null),
+      city:this.fb.control(""),
+      state:this.fb.control("")
+    }));
+  }
+
+  setNotification(notificationType:string){
+    let mobileNoControl = this.studentEditForm.get("mobileNo");
+    if (notificationType=="mobile") {
+      mobileNoControl.setValidators(Validators.required);
+    }else{
+      mobileNoControl.clearValidators();
+    }
+    mobileNoControl.updateValueAndValidity();
   }
 
 }
